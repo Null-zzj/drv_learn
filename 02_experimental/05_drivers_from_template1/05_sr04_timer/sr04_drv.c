6000f76b37ae6cd0ef76753732f037adf4734d51
Null-zzj <zj.zhu.cn@gmail.com>
2023-11-20 07:54:27 -0500

    sr04_drv_timer

--- 02_experimental/05_drivers_from_template1/05_sr04_timer/sr04_drv.c
+++ 02_experimental/05_drivers_from_template1/05_sr04_timer/sr04_drv.c
@@ -1,5 +1,9 @@
-﻿#include "linux/ktime.h"
+﻿#include "asm-generic/errno.h"
+#include "asm/signal.h"
+#include "linux/jiffies.h"
+#include "linux/ktime.h"
 #include "linux/timekeeping.h"
+#include "linux/wait.h"
 #include <asm-generic/gpio.h>
 #include <asm/gpio.h>
 #include <linux/delay.h>
@@ -98,7 +102,20 @@ static int get_val(void)
     return val;
 }
 
+
+
 static DECLARE_WAIT_QUEUE_HEAD(gpio_wait);
+
+// 定时器超时函数
+static void sr04_timer_func(unsigned long data)  
+{
+    put_val(-1);
+    wake_up_interruptible(&gpio_wait);
+    kill_fasync(&button_fasync, SIGIO, POLL_IN);
+
+
+
+}
 
 /* 实现对应的open/read/write等函数，填入file_operations结构体                   */
 static ssize_t sr04_drv_read(struct file *file, char __user *buf, size_t size, loff_t *offset)
@@ -112,18 +129,20 @@ static ssize_t sr04_drv_read(struct file *file, char __user *buf, size_t size, l
 
     wait_event_interruptible(gpio_wait, !is_val_buf_empty());
     val = get_val();
+    if(val == -1)
+        return -ENODATA;
+
     err = copy_to_user(buf, &val, 4);
 
     return 4;
 }
-
 
 // sys_polld调用
 static unsigned int sr04_drv_poll(struct file *fp, poll_table *wait)
 {
     // printk("%s %s line %d\n", __FILE__, __FUNCTION__, __LINE__);
     poll_wait(fp, &gpio_wait, wait);
-    return is_val_buf_empty() ? 0 : POLLIN | POLLRDNORM;  // 返回0 sys_pll 休眠一段时间
+    return is_val_buf_empty() ? 0 : POLLIN | POLLRDNORM; // 返回0 sys_pll 休眠一段时间继续调用
 }
 
 static int sr04_drv_fasync(int fd, struct file *file, int on)
@@ -143,6 +162,10 @@ long sr04_drv_ioctl(struct file *fp, unsigned int cmd, unsigned long arg)
         gpio_set_value(gpios[0].gpio, 1);
         udelay(20);
         gpio_set_value(gpios[0].gpio, 0);
+
+
+        // start timer
+        mod_timer(&gpios[1].key_timer, jiffies + msecs_to_jiffies(50));
     }
     }
     return 0;
@@ -180,6 +203,9 @@ static irqreturn_t gpio_sr04_isr(int irq, void *dev_id)
             return IRQ_HANDLED;
         }
         // 下降沿记录结束时间，并计算时间差 计算距离
+
+        // stop timer
+        del_timer(&gpios[1].key_timer);
         time = ktime_get_ns() - rising_time;
         rising_time = 0;
         put_val(time);
@@ -196,9 +222,6 @@ static irqreturn_t gpio_sr04_isr(int irq, void *dev_id)
 static int __init gpio_drv_init(void)
 {
     int err;
-
-    printk("%s %s line %d\n", __FILE__, __FUNCTION__, __LINE__);
-
     // trig pin
     err = gpio_request(gpios[0].gpio, gpios[0].name);
     if (err)
@@ -225,6 +248,7 @@ static int __init gpio_drv_init(void)
     err =
         request_irq(gpios[1].irq, gpio_sr04_isr, IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING, gpios[1].name, &gpios[1]);
 
+    setup_timer(&gpios[1].key_timer, sr04_timer_func, (unsigned long)&gpios[1]);
     /* 注册file_operations 	*/
     major = register_chrdev(0, "100ask_gpio_sr04", &gpio_sr04_drv); /* /dev/gpio_desc */
 
@@ -255,6 +279,7 @@ static void __exit gpio_drv_exit(void)
     gpio_free(gpios[0].gpio);
     gpio_free(gpios[1].gpio);
     free_irq(gpios[1].irq, &gpios[1]);
+    del_timer(&gpios[1].key_timer);
 }
 
 /* 7. 其他完善：提供设备信息，自动创建设备节点                                     */
